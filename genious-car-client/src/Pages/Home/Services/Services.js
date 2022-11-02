21bd9d3fb59fe4e492b002bea5bc99ab35d36ee5
Abir Hasan <cse.170201063@gmail.com>
2022-11-02 16:32:11 +0600

    states added

--- genious-car-client/src/Pages/Home/Services/Services.js
+++ genious-car-client/src/Pages/Home/Services/Services.js
@@ -10,7 +10,7 @@ const Services = () => {
   }, []);
     
   return (
-    <div>
+    <div className="my-28">
       <div className="text-center">
         <p className="text-2xl font-bold text-orange-600">Service</p>
         <h2 className="text-5xl font-semibold">Our Service Area</h2>
@@ -19,15 +19,17 @@ const Services = () => {
           humour, <br />
           or randomised words which don't look even slightly believable.
         </p>
-          </div>
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-              {
-                  services.map(service => <ServiceCard
-                      key={service._id}
-                      service={service}
-                  ></ServiceCard>)
-              }
-          </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service}></ServiceCard>
+        ))}
+      </div>
+      <div className="flex justify-center my-10">
+        <button className="btn btn-outline hover:bg-orange-600 text-orange-600">
+          See More
+        </button>
+      </div>
     </div>
   );
 };
